0240b10fa6cf0dea359a9e1b9b804b65ed2df5c7
KSUN5 <769296817@qq.com>
2025-03-24 13:37:21 -0500

    client

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,14 +1,31 @@
 package kvraft
 
-import "course/labrpc"
+import (
+	"course/labrpc"
+	"crypto/rand"
+	"math/big"
+)
 
 type Clerk struct {
+	servers []*labrpc.ClientEnd
+
+	leaderId  int
+	leaderSeq int
 }
 
 func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := rand.Int(rand.Reader, max)
+	x := bigx.Int64()
+	return x
 }
 
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
+	ck := new(Clerk)
+	ck.servers = servers
+	ck.leaderId = 0
+	ck.leaderSeq = 0
+	return ck
 }
 
 // Get fetch the current value for a key.
@@ -22,6 +39,18 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
+	args := GetArgs{
+		Key: key,
+	}
+	reply := GetReply{}
+	for {
+		ok := ck.servers[ck.leaderId].Call("KVServer.Get", &args, &reply)
+		if !ok || reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
+		}
+		return reply.Value
+	}
 }
 
 // PutAppend shared by Put and Append.
@@ -33,7 +62,20 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-
+	args := PutAppendArgs{
+		Key:   key,
+		Value: value,
+		Op:    op,
+	}
+	reply := PutAppendReply{}
+	for {
+		ok := ck.servers[ck.leaderId].Call("KVServer.PutAppend", &args, &reply)
+		if !ok || reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
+		}
+		return
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
