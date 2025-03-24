2d26c5e632cc22fddb3030a404ef242f9c617e15
sunkuangdong <769296817@qq.com>
2025-03-24 13:17:07 -0500

    init kv_raft system

---
+++ src/kvraft/client.go
@@ -0,0 +1,44 @@
+package kvraft
+
+import "course/labrpc"
+
+type Clerk struct {
+}
+
+func nrand() int64 {
+}
+
+func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
+}
+
+// Get fetch the current value for a key.
+// returns "" if the key does not exist.
+// keeps trying forever in the face of all other errors.
+//
+// you can send an RPC with code like this:
+// ok := ck.servers[i].Call("KVServer.Get", &args, &reply)
+//
+// the types of args and reply (including whether they are pointers)
+// must match the declared types of the RPC handler function's
+// arguments. and reply must be passed as a pointer.
+func (ck *Clerk) Get(key string) string {
+}
+
+// PutAppend shared by Put and Append.
+//
+// you can send an RPC with code like this:
+// ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+//
+// the types of args and reply (including whether they are pointers)
+// must match the declared types of the RPC handler function's
+// arguments. and reply must be passed as a pointer.
+func (ck *Clerk) PutAppend(key string, value string, op string) {
+
+}
+
+func (ck *Clerk) Put(key string, value string) {
+	ck.PutAppend(key, value, "Put")
+}
+func (ck *Clerk) Append(key string, value string) {
+	ck.PutAppend(key, value, "Append")
+}
